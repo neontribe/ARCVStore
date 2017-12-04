db507c5291ce1c8c3e283b47e7dc0db7a9f7c3c2
Charles Strange <charles@neontribe.co.uk>
2017-12-04 12:28:00 +0000

    Generate initial models Create initial migrations.

---
+++ database/migrations/2017_12_04_113343_create_registrations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRegistrationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('registrations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('cc_reference'); // The Children's Centre system id
+            $table->string('eligibility');
+            $table->dateTime('consented_on')->nullable();
+            $table->dateTime('fm_chart_on')->nullable();
+            $table->dateTime('fm_diary_on')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('registrations');
+    }
+}
