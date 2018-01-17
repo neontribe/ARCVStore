ad51336ddc38f153d03274bfb3edfe253c76c745
Alexandra Moore <alexmoore94@hotmail.co.uk>
2018-01-17 16:27:35 +0000

    logo link test aded but still brok

--- resources/views/service/partials/masthead.blade.php
+++ resources/views/service/partials/masthead.blade.php
@@ -1,5 +1,5 @@
 <div class="header">
-    <img src="{{ asset('assets/logo.png') }}">
+    <img src="{{ asset('assets/logo.png') }}" name="logo">
     <h1>Rose Vouchers</h1>
     <ul>
         <li>User: {{ $user_name }}</li>
---
+++ tests/Feature/RegistrationPageTest.php
@@ -0,0 +1,53 @@
+<?php
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class RegistrationPageTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function iCanRegisterAFamily()
+    {
+        // // Create a centre
+        // factory(App\Centre::class)->create();
+
+        // // Create a User
+        // $user =  factory(App\User::class)->create([
+        //     "name"  => "test user",
+        //     "email" => "testuser@example.com",
+        //     "password" => bcrypt('test_user_pass'),
+        //     "centre_id" => 1,
+        // ]);
+
+        // // Fill out minimum on family page and submit
+        // $this->actingAs($user)
+        //     ->visit(URL::route('service.registration.create'))
+        //     ->type('Test Main Carer', '#carer')
+        //     ->press('Save Family')
+        //     ->see(URL::route('service.registration.edit'));
+
+        //To do: POST registration to the DB, test seeInDatabase that it's there
+    }
+
+    /** @test */
+    public function logoDoesntRedirectMeToDashboard()
+    {
+        // Create some centres
+        factory(App\Centre::class, 4)->create();
+
+        // Create a User
+        $user =  factory(App\User::class)->create([
+            "name"  => "test user",
+            "email" => "testuser@example.com",
+            "password" => bcrypt('test_user_pass'),
+            "centre_id" => 1,
+        ]);
+
+        $this->actingAs($user)
+            ->visit(URL::route('service.registration.index'))
+            ->click('logo')
+            ->seePageIs(URL::route('service.registration.index'));
+    }
+}
