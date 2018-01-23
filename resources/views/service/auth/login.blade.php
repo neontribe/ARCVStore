99e9356d118284af1e6f344c0077f6419658340a
Charles Strange <charles@neontribe.co.uk>
2018-01-23 10:18:35 +0000

    Made some changes to get closer to Designer's ideals.

--- resources/views/service/auth/login.blade.php
+++ resources/views/service/auth/login.blade.php
@@ -3,40 +3,42 @@
 @section('title', 'Login')
 
 @section('content')
-<div class="login">
-    <h2>Log In</h2>
-    @if ($errors->has('error_message'))
-        <div class="alert alert-danger">
-            <strong>{{ $errors->first('error_message') }}</strong>
-        </div>
-    @endif
-    <form role="form" method="POST" action="{{ route('service.login') }}">
-        {{ csrf_field() }}
-        <div>
-            <label for="email">Email Address</label>
-            <input id="email" type="email" class="login-input" name="email" value="{{ old('email') }}" required autofocus>
-            @if ($errors->has('email'))
-                <span class="help-block">
+    <div class="content">
+        <div class="login">
+            <h2>Log In</h2>
+            @if ($errors->has('error_message'))
+                <div class="alert alert-danger">
+                    <strong>{{ $errors->first('error_message') }}</strong>
+                </div>
+            @endif
+            <form role="form" method="POST" action="{{ route('service.login') }}">
+                {{ csrf_field() }}
+                <div>
+                    <label for="email">Email Address</label>
+                    <input id="email" type="email" class="login-input" name="email" value="{{ old('email') }}" required
+                           autofocus>
+                    @if ($errors->has('email'))
+                        <span class="help-block">
                     <strong>{{ $errors->first('email') }}</strong>
                 </span>
-            @endif
-        </div>
-        <div>
-            <label for="password">Password</label>
-                <input id="password" class="login-input" type="password" name="password" required>
-                @if ($errors->has('password'))
-                    <span class="help-block">
+                    @endif
+                </div>
+                <div>
+                    <label for="password">Password</label>
+                    <input id="password" class="login-input" type="password" name="password" required>
+                    @if ($errors->has('password'))
+                        <span class="help-block">
                         <strong>{{ $errors->first('password') }}</strong>
                     </span>
-                @endif
+                    @endif
+                </div>
+                <div class="remember">
+                    <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}>
+                    <label>Remember Me</label>
+                </div>
+                <button type="submit">Log In</button>
+                <a href="{{ route('password.request') }}">Forgot Your Password?</a>
+            </form>
         </div>
-        <div class="remember">
-            <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}>
-            <label>Remember Me</label>
-        </div>
-        <button type="submit">Log In</button>
-        <a href="{{ route('password.request') }}">Forgot Your Password?</a>
-    </form>
-</div>
-
+    </div>
 @endsection
