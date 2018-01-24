5b2a0366f3a86cb658e50a90b45057e6a77b49d9
Alexandra Moore <alexmoore94@hotmail.co.uk>
2018-01-24 20:45:04 +0000

    weekly sheet added and set up

---
+++ resources/views/service/printables/weekly_families_sheet.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.printable_master')
+
+<link rel="stylesheet" type="text/css" href="{{ asset('css/print.css') }}">
+
+@section('title', $sheet_title)
+
+@section('content')
+
+<div class="content">
+</div>
