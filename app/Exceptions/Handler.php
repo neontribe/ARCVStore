513bd1be37e332eb165d09f4f1d2b8c4853ebec6
Charles Strange <charles@neontribe.co.uk>
2018-02-01 18:00:43 +0000

    Alter auth to give friendly messages

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -5,6 +5,7 @@ namespace App\Exceptions;
 use Exception;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Session\TokenMismatchException;
 
 class Handler extends ExceptionHandler
 {
@@ -44,6 +45,14 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($exception instanceof TokenMismatchException) {
+            //Explicitly redirect to login form if token mismatches
+            return redirect()
+                ->guest(route('service.login'))
+                // We anticipate expiry to be the most common reason.
+                ->withErrors(['error_message' => trans('auth.expired')])
+                ;
+        }
         return parent::render($request, $exception);
     }
 
@@ -60,6 +69,8 @@ class Handler extends ExceptionHandler
             return response()->json(['error' => 'Unauthenticated.'], 401);
         }
 
-        return redirect()->guest(route('service.login'));
+        return redirect()
+            ->guest(route('service.login'))
+            ->withErrors(['error_message' => trans('auth.exception')]);
     }
 }
