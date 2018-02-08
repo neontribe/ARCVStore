c37f0c14ece3e4c9ac1a8a1fb5cdde9e3c712f56
Charles Strange <charles@neontribe.co.uk>
2018-02-08 15:13:41 +0000

    tests

---
+++ tests/Unit/Models/RegistrationModelTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests;
+
+use App\Centre;
+use App\Registration;
+use App\Sponsor;
+use App\User;
+use Carbon\Carbon;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Support\Collection;
+
+class RegistrationModelTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function itCanReturnRegistrationsOnlyForActiveFamilies()
+    {
+        // Create a centre
+        $centre = factory(Centre::class)->create();
+
+        // Create 4 random registrations (and families etc.) in that centre.
+        $registrations = factory(Registration::class, 4)->create([
+            'centre_id' => $centre->id,
+        ]);
+
+        // Check that we have 4.
+        $this->assertEquals(Registration::whereActiveFamily()->count(), 4);
+
+        // A family has left.
+        $family = $registrations->first()->family;
+        $family->leaving_on = Carbon::now();
+        $family->save();
+
+        // check there are only 3.
+        $this->assertEquals(Registration::whereActiveFamily()->count(), 3);
+    }
+}
