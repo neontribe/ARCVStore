4d4da4a70205055a1fda717a64025639ca1dd334
Alexandra Moore <alexmoore94@hotmail.co.uk>
2018-04-04 10:31:58 +0100

    cookie notice added to top of page and styled

--- resources/views/service/auth/login.blade.php
+++ resources/views/service/auth/login.blade.php
@@ -5,6 +5,11 @@
 @section('hoist-head')
     <!-- specifically to avoid login page timeout -->
     <meta http-equiv="refresh" content="{{ (config('session.lifetime') * 60) - 30 }};url={{ route('service.login') }}" />
+    
+    <div class="cookie-notice">
+        <p>We use cookies to authenticate you so we can ensure that we give you the best experience on our website. For more information please read our <a>Privacy Policy</a>.</p>
+        <button>I agree</button>
+    </div>
 @endsection
 
 @section('content')
